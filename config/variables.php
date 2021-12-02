e7803836c7f3c76507e3921c3f25079f1cf1a1d8
KemoXtech <93053669+KemoXtech@users.noreply.github.com>
2021-12-02 03:55:37 +0000

    Update variables.php

--- config/variables.php
+++ config/variables.php
@@ -10,23 +10,6 @@ $username = $update["message"]["from"]["username"];
 $firstname = $update["message"]["from"]["first_name"];
 $start_msg = $_ENV['START_MSG']; 
 
-$replytomessageis = $update->message->reply_to_message->text;
-
-$data = $update->callback_query->data;
-
-$callbackfname = $update->callback_query->from->first_name;
-
-$callbacklname = $update->callback_query->from->last_name;
-
-$callbackusername = $update->callback_query->from->username;
-
-$callbackchatid = $update->callback_query->message->chat->id;
-
-$callbackuserid = $update->callback_query->message->reply_to_message->from->id;
-
-$callbackmessageid = $update->callback_query->message->message_id;
-
-
 $live_array = array(
     'incorrect_cvc', 
     '"cvc_check": "fail"', 
