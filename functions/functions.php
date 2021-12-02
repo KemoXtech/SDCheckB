5361d6a0df5963522a989bcec1b2782470fd6461
KemoXtech <93053669+KemoXtech@users.noreply.github.com>
2021-12-02 02:16:56 +0000

    Update functions.php

--- functions/functions.php
+++ functions/functions.php
@@ -1,7 +1,7 @@
 <?php
 
-include dirname(__FILE__)."/../config/config.php";
-include_once dirname(__FILE__) ."/../functions/bot.php";
+require dirname(__FILE__)."/../../config/config.php";
+require_once dirname(__FILE__)."/../../functions/bot.php";
 
 function capture($string, $start, $end)
 {
